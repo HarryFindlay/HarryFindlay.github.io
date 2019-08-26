88b91d74b2bae47bfc759bd82a906eaa4b1d2a5f
HarryFindlay <54514727+HarryFindlay@users.noreply.github.com>
2019-08-26 16:16:57 +0100

    reverting to click, also for mobile

--- cs.js
+++ cs.js
@@ -1,11 +1,10 @@
 var ifImage = document.querySelector(".if-learning-container > img");
 
-/*
-ifImage.addEventListener("mouseenter", function( event ) {   
+
+ifImage.addEventListener("click", function( event ) {   
     ifImage.src = "images/CS/CS smallest num of 3 console.PNG";
 
     setTimeout(function() {
       ifImage.src = "images/CS/CS smallest num of 3.PNG";
     }, 5000);
   }, false);
-*/
