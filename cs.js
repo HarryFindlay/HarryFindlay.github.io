47dc7d7551755e48e692b19ff48c7bac38a8a0bc
HarryFindlay <54514727+HarryFindlay@users.noreply.github.com>
2019-08-26 22:34:28 +0100

    adding another click func to image

--- cs.js
+++ cs.js
@@ -8,3 +8,15 @@ ifImage.addEventListener("click", function() {
     ifImage.src = "images/CS/CS smallest num of 3.PNG";
   }, 5000);
 }, false);
+
+
+var heightConverterImg = document.querySelector(".height-converter-container > img");
+
+heightConverterImg.addEventListener("click", function() {   
+  ifImage.src = "images/CS/Height Converter Console.PNG";
+
+  setTimeout(function() {
+    ifImage.src = "images/CS/Height Converter.PNG";
+  }, 5000);
+}, false);
+
