7fce31bba5975cadbe36bef70caacbb46ae7419e
HarryFindlay <54514727+HarryFindlay@users.noreply.github.com>
2019-08-26 16:21:23 +0100

    change the sytax of cs js

--- cs.js
+++ cs.js
@@ -1,7 +1,15 @@
 var ifImage = document.querySelector(".if-learning-container > img");
 
-
+/*
 ifImage.addEventListener("click", function( event ) {   
+    ifImage.src = "images/CS/CS smallest num of 3 console.PNG";
+
+    setTimeout(function() {
+      ifImage.src = "images/CS/CS smallest num of 3.PNG";
+    }, 5000);
+  }, false);
+*/
+  ifImage.addEventListener("click", function() {   
     ifImage.src = "images/CS/CS smallest num of 3 console.PNG";
 
     setTimeout(function() {
