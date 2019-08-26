57a3e31488929da3de75f9e30ddaa2ee792e72e1
HarryFindlay <54514727+HarryFindlay@users.noreply.github.com>
2019-08-26 15:56:15 +0100

    adding mouseover func

--- cs.js
+++ cs.js
@@ -1,9 +1,6 @@
 var ifImage = document.querySelector(".if-learning-container > img");
 
-ifImage.addEventListener("click", () => {
+ifImage.addEventListener("mouse-over", () => {
     ifImage.src = "images/CS/CS smallest num of 3 console.PNG";
-
-    if(ifImage.src == "images/CS/CS smallest num of 3 console.PNG"){
-        ifImage.src = "images/CS/CS smallest num of 3.PNG";
-    }
 });
+
